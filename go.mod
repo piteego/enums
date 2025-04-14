1dfa9bc6122b85884e939fcd925480c61dbf6a33
tirouzh <ctirouzh@gmail.com>
2025-04-15 00:38:35 +0330

    init code generator command

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/piteego/enums
 
 go 1.23.3
+
+require golang.org/x/text v0.24.0
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"github.com/piteego/enums/cmd"
+	"log"
+)
+
+func main() {
+	var (
+		// __type is the flag for the enum type name
+		__type string
+		// __output is the flag for the enum output file name
+		//__output string
+	)
+	flag.StringVar(&__type, "type", "", "the name of the type")
+	//flag.StringVar(&__output, "output", "", "the name of the output file")
+	flag.Parse()
+	if __type == "" {
+		log.Fatalf("Type name is required")
+	}
+	if err := cmd.Generate(__type); err != nil {
+		log.Fatalf("Failed to generate code for %s enum: %v\n", __type, err)
+	}
+}
